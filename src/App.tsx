5186f58608dc29b226e6d6f3ad28dc76513ecd59
gustavo gomes <ggomes@grupodimed.com.br>
2022-03-29 20:14:52 -0300

    feat: adicionado menu-lateral

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 
-import { BrowserRouter } from "react-router-dom"
-import { AppRoutes } from "./routes";
-import { AppThemeProvider } from "./shared/contexts/ThemeContext";
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './routes';
+import { MenuLateral } from './shared/components';
+import { AppThemeProvider } from './shared/contexts/ThemeContext';
 
 
 export const App =() => {
   return (
-    
-  <AppThemeProvider>
-    <BrowserRouter>   
-       <AppRoutes/>
-    </BrowserRouter>
-  </AppThemeProvider>
-    
+      
+    <AppThemeProvider>
+      <BrowserRouter>  
 
-   
+      <MenuLateral>
+         <AppRoutes/>  
+      </MenuLateral> 
+      
+      </BrowserRouter>
+    </AppThemeProvider>
+  
   );
-}
+};
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,12 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { useAppThemeContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
-  const {toggleTheme } = useAppThemeContext();
+  const { toggleTheme } = useAppThemeContext();
+
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toggleTheme} >GUSTAVO</Button>} />
-
+      <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toggleTheme} >TESTES</Button>} />
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
   );
-}
+};
